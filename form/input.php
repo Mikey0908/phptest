2afd3a7a5fdbe1aca3f2ba6d456c3a42b0c8299b
EL93019205 <el93019205_2@yahoo.co.jp>
2021-06-30 05:06:02 +0900

    33 フォームGET,POST

--- form/input.php
+++ form/input.php
@@ -1,11 +1,27 @@
+<?php
+
+// スーパーグローバル変数 9種類ある 連想配列になっている
+if( !empty($_POST) ){
+  echo '<pre>';
+  var_dump($_POST);
+  echo '</pre>';
+}
+
+?>
+
 <!DOCTYPE html>
 <meta charset="utf-8">
 <head></head>
 <body>
 
-<form method="GET" action="input.php">
+<form method="POST" action="input.php">
 氏名
 <input type="text" name="your_name">
+
+<br>
+<input type="checkbox" name="sports[]" value="野球">野球
+<input type="checkbox" name="sports[]" value="サッカー">サッカー
+<input type="checkbox" name="sports[]" value="バスケ">バスケ
 
 <input type="submit" value="送信">
 
