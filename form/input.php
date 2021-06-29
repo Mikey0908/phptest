5fb155c8c55072548c52554f9ba69cbef4570850
EL93019205 <el93019205_2@yahoo.co.jp>
2021-06-30 04:53:25 +0900

    33 form作成

---
+++ form/input.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head></head>
+<body>
+
+<form method="GET" action="input.php">
+氏名
+<input type="text" name="your_name">
+
+<input type="submit" value="送信">
+
+
+</form>
+
+</body>
+</html>
