6c758b5e1c83ce43e604d9562ac4b3eb3120e648
EL93019205 <el93019205_2@yahoo.co.jp>
2021-07-03 19:56:49 +0900

    45 ファイル操作 その1

--- mainte/test.php
+++ mainte/test.php
@@ -1,11 +1,28 @@
 <?php
 
-// パスワードを記録したファイルの場所
-echo __FILE__;
+$contactFile = '.contact.dat';
 
-echo '<br>';
+// ファイル丸ごと読み込み
+$fileContents = file_get_contents($contactFile);
+echo $fileContents.'<br>';
 
-// パスワード(暗号化)
-echo(password_hash('password123', PASSWORD_BCRYPT ));
+// ファイルに書き込み(上書き)
+// file_put_contents($contactFile, "テストです"."\n");
+
+// ファイルに書き込み(追記)
+// file_put_contents($contactFile, $addText, FILE_APPEND);
+
+// 配列 file ,区切る explode foreach
+
+$allData = file($contactFile);
+
+foreach( $allData as $lineData )
+{
+  $lines = explode(',', $lineData);
+  echo $lines[0].' ';
+  echo $lines[1].' ';
+  echo $lines[2].' ';
+  echo $lines[3].'<br>';
+}
 
 ?>
