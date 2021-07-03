1ea8fb758912b8bc6edb67f38c3f0fc7e83a2e15
EL93019205 <el93019205_2@yahoo.co.jp>
2021-07-03 20:23:30 +0900

    46 ファイル操作 その2

--- mainte/test.php
+++ mainte/test.php
@@ -2,27 +2,10 @@
 
 $contactFile = '.contact.dat';
 
-// ファイル丸ごと読み込み
-$fileContents = file_get_contents($contactFile);
-echo $fileContents.'<br>';
+$contents = fopen($contactFile, 'a+' );
+$addText = '1行追記' . "\n";
 
-// ファイルに書き込み(上書き)
-// file_put_contents($contactFile, "テストです"."\n");
-
-// ファイルに書き込み(追記)
-// file_put_contents($contactFile, $addText, FILE_APPEND);
-
-// 配列 file ,区切る explode foreach
-
-$allData = file($contactFile);
-
-foreach( $allData as $lineData )
-{
-  $lines = explode(',', $lineData);
-  echo $lines[0].' ';
-  echo $lines[1].' ';
-  echo $lines[2].' ';
-  echo $lines[3].'<br>';
-}
+fwrite($contents, $addText);
+fclose($contents);
 
 ?>
